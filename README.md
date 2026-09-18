9ad69a3ac02544dff870ec9cc70426deecd8248f
AeroX-OS <329861041+AeroX-OS@users.noreply.github.com>
2026-09-18 14:28:32 +1000

    License, README and load_root
    read_sectors does not exist despite being called, will crash. Refer to previous commit for working version.

---
+++ README.md
@@ -0,0 +1,7 @@
+# AeroX Disk Operating System
+
+**AeroX** is a free and open-source operating system designed purely as a learning experience in order to help people better understand **Assembly** and **C**. We do not have a long term goal. We encourage everyone to open pull requests to contribute to our repository.
+
+## Requirements
+
+Basically doesn't need anything to run. **NASM** for compiling and **QEMU** for testing. This will be updated as we add C elements such as the kernel.
--- boot.asm
+++ boot.asm
@@ -7,6 +7,9 @@
 ;
 ;   Change history:
 ;   ---------------
+;   Riley, 18-09-2026:
+;       Worked on load_root function
+;   
 ;   Riley, 17-09-2026:
 ;       First major bootloader rewrite
 ;
@@ -63,5 +66,100 @@ cluster_lba:
     mov cl, BYTE [bpbSectorsPerCluster]     ;   find out how many sectors there are in a single cluster
     mul cx                                  ;   in 16-bit, mul targets AX, so this would be AX * CX
                                             ;   (e.g. if cluster was 2, AX was 2, now 0 * sectors = 0 offset)
-    add ax, word [datasector]               ;   add the global data area offset to get the absolute sector
+    add ax, WORD [datasector]               ;   add the global data area offset to get the absolute sector
     ret
+
+lbachs:
+    xor dx, dx                              ;   clear dx before division
+    div WORD [bpbSectorsPerTrack]           ;   div DX:AX by sectors per track (18)
+    inc dl                                  ;   sectors start at 1 (remainders at 0)
+    mov BYTE [absoluteSector], dl           ;   save the sector number
+
+    xor dx, dx                              ;   clear dx again for div
+    div WORD [bpbHeadsPerCylinder]          ;   div track count by heads per cylinder (2)
+    mov BYTE [absoluteHead], dl             ;   remainder is head number
+    mov BYTE [absoluteTrack], al
+    ret
+
+read_sectors:
+.main:
+    mov di, 0x0005  ;   read 5 times before fail
+.sector_loop:
+    push ax
+    push bx
+    push cx
+    call lbachs
+    mov ah, 0x02
+    mov al, 0x01
+    mov ch, BYTE [absoluteTrack]
+    mov cl, BYTE [absoluteSector]
+    mov dh, BYTE [absoluteHead]
+    mov dl, BYTE [bsDriveNumber]
+    int 0x13
+    jnc .success
+
+    dec di
+    pop cx
+    pop bx
+    pop ax
+    jnz .sector_loop
+    int 0x18
+.success:
+    mov si, msg_progress
+    call print
+    pop cx
+    pop bx
+    pop ax
+    add bx, WORD [bpbBytesPerSector]
+    jnc .next
+
+    mov dx, es
+    cmp dx, 0x2000
+    jne .wrap_fail
+    jmp failure
+.wrap_fail:
+    int 0x18
+.next:
+    inc ax
+    loop .main
+    ret
+
+load_root:
+    xor cx, cx                          ;   clear cx and dx
+    xor dx, dx
+    mov ax, 0x0020
+    mul WORD [bpbRootEntries]           ;   for reference WORD is 2 bytes (16 bits)
+    div WORD [bpbBytesPerSector]
+    xchg ax, cx                         ;   exchanges the contents of the destination, AX (0x0020), with the source, CX, 0)
+    mov al, BYTE [bpbNumberofFATs]
+    mul WORD [bpbNumberofFATs]
+    add ax, WORD [bpbReservedSectors]
+    mov WORD [datasector], ax
+    add WORD [datasector], cx
+    mov bx, 0x0200
+    call read_sectors
+
+main:
+    cli
+    mov ax, 0x7C00
+    mov es, ax
+    mov ax, 0x0000
+    mov ss, ax
+    mov sp, 0xFFFF
+    sti
+    mov si, msg_loading
+    call print
+
+failure:
+    mov si, msg_failure
+    call print
+
+datasector dw 0x0000
+cluster dw 0x0000
+
+msg_loading db 0x0D, 0x0A, "Perchance, but of course", 0x0D, 0x0A, 0x00
+msg_progress db ".", 0x00
+msg_failure db 0x0D, 0x0A, "Kernel not found", 0x0D, 0x0A, 0x00
+
+TIMES 510-($-$$) DB 0
+DW 0xAA55
